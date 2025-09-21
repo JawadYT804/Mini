af9764652e1004a93abe6b4e1bfceb658409acff
JawadYT804 <jadixop@gmail.com>
2025-09-21 06:23:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+__path = process.cwd()
 const bodyParser = require("body-parser");
-
-const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
+let code = require('./pair'); 
 
-// Increase event listeners
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-// Middleware
+app.use('/code', code);
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
+});
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Static files
-app.use(express.static(path.join(__path, 'public')));
-
-// Routes
-app.use('/code', require('./pair'));
-app.use('/pair', (req, res) => {
-    res.sendFile(path.join(__path, 'main.html'));
-});
-
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__path, 'main.html'));
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-// 404 handler
-app.use((req, res) => {
-    res.status(404).send('Page not found');
-});
-
 app.listen(PORT, () => {
-    console.log(`SIGMA-MD Server running on http://localhost:${PORT}`);
+    console.log(`SIGMA-MD Server running on http://localhost:` + PORT)
 });
 
 module.exports = app;
